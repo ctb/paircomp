704bd83adcd97bae2804fc45f4dfa92b6be7087b
t <t@d855f457-450e-0410-a4f6-b2c5563b6eec>
2006-11-29 16:23:56 +0000

    removed tests

--- tests/README.txt
+++
@@ -1,5 +0,0 @@
-Testing for paircomp, done entirely via the Python interface.
-
-Execute './run-tests.sh' to run all of the tests.
-
-CTB 1/05
--- tests/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-./test-python-interface.py && \
-./test-regress.py && \
-./test-transitive.py && \
-./test-transitive-2.py && \
-./test-compare-mussa.py && \
-./test-errors.py
-/bin/rm -f *.cmp
